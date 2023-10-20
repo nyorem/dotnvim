7221c5d39d62fffa9f187367de18d03253dab54b
Meyron, Jocelyn <JocelynMeyron@eaton.com>
2023-10-20 08:50:44 +0200

    initial commit

---
+++ lua/plugins.lua
@@ -0,0 +1,48 @@
+-- {{{1 VIM-PLUG
+local Plug = vim.fn["plug#"]
+
+vim.call("plug#begin", "~/.nvim/bundle")
+
+Plug 'nvim-lua/plenary.nvim'
+Plug 'NeogitOrg/neogit'
+
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'nvim-telescope/telescope-file-browser.nvim'
+Plug 'nvim-telescope/telescope-project.nvim'
+
+Plug 'sindrets/diffview.nvim'
+Plug 'ibhagwan/fzf-lua'
+
+Plug 'catppuccin/nvim'
+
+vim.call("plug#end")
+
+-- {{{1 NEOGIT
+local neogit = require("neogit")
+neogit.setup {}
+vim.keymap.set('n', '<Space>gg', ':Neogit<CR>')
+
+-- {{{1 STUFF
+vim.cmd.colorscheme "catppuccin"
+
+vim.keymap.set('n', '<Space>pp', ':Telescope project<CR>')
+
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_cursor_animate_in_insert_mode = false
+  vim.g.neovide_cursor_animate_command_line = false
+
+  vim.g.coc_node_path = vim.env.HOME .."/.nvm/versions/node/v18.14.1/bin/node"
+
+  vim.keymap.set({'i', 'c'}, '<C-v>', '<c-r>+')
+  vim.keymap.set({'n', 'v'}, '<C-c>', '"+y')
+end
+
+-- {{{1 TELESCOPE
+require('telescope').setup {
+  extensions = {
+    project = {
+      order_by = "asc",
+    }
+  }
+}
