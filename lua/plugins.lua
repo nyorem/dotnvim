94eafffdf44fbf9af12c77385d36620d58f00d42
Jocelyn Meyron <jmeyron@gmail.com>
2024-08-28 20:21:35 +0200

    feat: customize gitsigns

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -149,6 +149,13 @@ vim.keymap.set('n', '<Space>tn', ':tab terminal<CR>')
 
 -- {{{1 gitsigns
 require('gitsigns').setup {
+  signs = {
+    add = { text = '+' },
+    change = { text = '>' },
+    delete = { text = '-' },
+    topdelete = { text = '^' },
+    changedelete = { text = '<' },
+  },
   current_line_blame_opts = {
     delay = 0,
   }
