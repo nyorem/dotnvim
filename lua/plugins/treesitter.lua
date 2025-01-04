b3fd94c3962a556fb09f7985261d692d254601a9
Meyron, Jocelyn <JocelynMeyron@eaton.com>
2025-01-04 16:08:44 +0100

    fix: disable treesitter for bitbake

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,11 +3,12 @@ return {
   build = ":TSUpdate",
   config = function()
     require('nvim-treesitter.configs').setup {
-      ensure_installed = { "bash", "bitbake", "c", "cpp", "json", "lua", "markdown", "markdown_inline", "python", "query", "regex", "vim", "vimdoc" },
+      ensure_installed = { "bash", "c", "cpp", "json", "lua", "markdown", "markdown_inline", "python", "query", "regex", "vim", "vimdoc" },
       -- auto_install = true,
       highlight = {
         enable = true,
         disable = function(lang, buf)
+            if lang == "bitbake" then return true end
             local max_filesize = 100 * 1024 -- 100 KB
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
