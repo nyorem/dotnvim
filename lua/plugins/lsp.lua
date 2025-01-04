47d04bc19d03ab77e6561938d5b2219db2b4c554
Meyron, Jocelyn <JocelynMeyron@eaton.com>
2025-01-04 16:25:08 +0100

    feat: use trouble.nvim for LSP diagnostics

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,47 +1,70 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "saghen/blink.cmp",
-    'stevearc/conform.nvim',
-    {
-      "folke/lazydev.nvim",
-      ft = "lua",
-      opts = {
-        library = {
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "saghen/blink.cmp",
+      'stevearc/conform.nvim',
+      {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
         },
       },
     },
-  },
-  config = function()
-    -- blink
-    local capabilities = require('blink.cmp').get_lsp_capabilities()
+    config = function()
+      -- disable virtual text for diagnostics by default
+      vim.diagnostic.config {
+        virtual_text = false,
+        underline = false,
+        signs = true,
+      }
 
-    -- lua
-    require("lspconfig").lua_ls.setup { capabilities = capabilities }
+      -- blink
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-    -- c/cpp
-    require("lspconfig").clangd.setup {
-      cmd = {
-        "clangd",
-        "-j=1",
-        "-background-index",
-        "--malloc-trim",
-      },
-      capabilities = capabilities,
-    }
+      -- lua
+      require("lspconfig").lua_ls.setup { capabilities = capabilities }
 
-    -- key mappings
-    vim.keymap.set("n", "<Space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-    vim.keymap.set("n", "<Space>cd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-    vim.keymap.set("n", "<Space>cr", "<cmd>lua vim.lsp.buf.references()<CR>")
-
-    -- code formatting
-    require("conform").setup({
-        formatters_by_ft = {
-          cpp = { "clang-format" },
+      -- c/cpp
+      require("lspconfig").clangd.setup {
+        cmd = {
+          "clangd",
+          "-j=1",
+          "-background-index",
+          "--malloc-trim",
         },
-    })
-    vim.keymap.set({"n", "v"}, "<Space>cf", function() require("conform").format() end)
-  end,
+        capabilities = capabilities,
+      }
+
+      -- key mappings
+      vim.keymap.set("n", "<Space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+      vim.keymap.set("n", "<Space>cd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+      vim.keymap.set("n", "<Space>cr", "<cmd>lua vim.lsp.buf.references()<CR>")
+
+      -- code formatting
+      require("conform").setup({
+          formatters_by_ft = {
+            cpp = { "clang-format" },
+          },
+      })
+      vim.keymap.set({"n", "v"}, "<Space>cf", function() require("conform").format() end)
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    keys = {
+      {
+        "<Space>cm",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+    },
+    config = function()
+      require("trouble").setup({})
+    end
+  }
 }
