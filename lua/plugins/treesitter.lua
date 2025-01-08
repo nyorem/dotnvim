4fd90ebdfb1ad1a118a5b9a2402095f3889ae8ba
Meyron, Jocelyn <JocelynMeyron@eaton.com>
2025-01-08 08:30:50 +0100

    feat: add yaml treesitter parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require('nvim-treesitter.configs').setup {
-      ensure_installed = { "bash", "c", "cpp", "json", "lua", "markdown", "markdown_inline", "python", "query", "regex", "vim", "vimdoc" },
+      ensure_installed = { "bash", "c", "cpp", "comment", "json", "lua", "markdown", "markdown_inline", "python", "query", "regex", "vim", "vimdoc", "yaml" },
       -- auto_install = true,
       highlight = {
         enable = true,
