d043c407fb16739e6d08accb419d3d4bea6f8593
Meyron, Jocelyn <JocelynMeyron@eaton.com>
2025-09-08 13:07:29 +0200

    feat: add mapping to delete buffers from telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -21,6 +21,18 @@ return {
           auto_quoting = false,
         },
       },
+      pickers = {
+        buffers = {
+          mappings = {
+            i = {
+              ["<c-d>"] = "delete_buffer",
+            },
+            n = {
+              ["d"] = "delete_buffer",
+            },
+          },
+        },
+      },
     }
 
     -- extensions
