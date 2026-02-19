476d38f5168f76eb5610c6aa8f6688ea1d537082
Meyron, Jocelyn <JocelynMeyron@eaton.com>
2026-02-19 20:10:01 +0100

    feat: add -x option to shellcheck to allow other scripts to be sourced

--- after/ftplugin/sh.lua
+++ after/ftplugin/sh.lua
@@ -1,1 +1,2 @@
-vim.keymap.set("n", "<F7>", "<cmd>ShellCheck!<CR>", { buffer = true })
+-- '-x' allows ShellCheck to follow sourced files, which is important for scripts that use 'source' or '.' to include other scripts.
+vim.keymap.set("n", "<F7>", "<cmd>ShellCheck! -x<CR>", { buffer = true })
